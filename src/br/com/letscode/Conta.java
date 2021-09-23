c8594d9642efc79b303df570a064c2460d599921
vcosmusjoao <rm88372@fiap.com.br>
2021-09-23 11:20:39 -0300

    Criação de Getters and Setters.

--- src/br/com/letscode/Conta.java
+++ src/br/com/letscode/Conta.java
@@ -17,6 +17,36 @@ public class Conta {
         this.nome = nome;
         this.saldo = saldo;
     }
-    
 
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
 }
