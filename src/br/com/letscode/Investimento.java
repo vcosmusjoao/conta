341e1eb75763f17ec40b5836c59a0dcdd1a15731
Lauredi Gustavo <68924576+LaurediGustavo@users.noreply.github.com>
2021-09-25 17:33:41 -0300

    commit teste

--- src/br/com/letscode/Investimento.java
+++ src/br/com/letscode/Investimento.java
@@ -8,7 +8,6 @@ public class Investimento extends Conta {
     public Investimento(TipoCliente tipoCliente, int numConta, String nome, double saldo) {
         super(tipoCliente, numConta, nome, saldo);
     }
-
     public void sacar(double valor){
         if(valor > 0){
             super.saldo += valor;
